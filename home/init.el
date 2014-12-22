9d0502a29778754c3df386e964ef7488696e0bc2
Paul Weaver <pauweave@cisco.com>
2014-12-22 12:59:02 +0000

    Handle column highlighting better

--- home/init.el
+++ home/init.el
@@ -25,8 +25,16 @@
 (global-hl-line-mode 1)
 
 (column-number-mode 1) ; in mode line
-(column-enforce-mode 1)
+(require 'column-enforce-mode)
+(global-column-enforce-mode 1)
 (add-hook 'python-mode-hook (lambda () (column-enforce-column 79)))
+(require 'fill-column-indicator)
+; fci-mode doesn't provide its own global setting
+(define-globalized-minor-mode global-fci-mode fci-mode turn-on-fci-mode)
+(global-fci-mode 1)
+(setq-default fill-column 80)
+(setq fci-rule-character ?│)
+(setq fci-rule-character-color "grey15")
 
 (show-smartparens-global-mode +1)
 ; (global-git-gutter-mode +1)
@@ -45,6 +53,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(error ((t (:foreground "color-161" :weight bold))))
  '(hl-line ((t (:background "grey15"))))
  '(linum ((t (:inherit shadow :background "grey15"))))
  '(region ((t (:background "color-17"))))
