f947f2868c707977e4043f8e29521ea1cf7d1227
Paul Weaver <pauweave@cisco.com>
2015-07-17 09:58:52 +0100

    Improve git diff mode colours

--- home/init.el
+++ home/init.el
@@ -89,6 +89,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(diff-added ((t (:inherit diff-changed :foreground "green"))))
+ '(diff-removed ((t (:inherit diff-changed :foreground "red"))))
  '(error ((t (:foreground "color-161" :weight bold))))
  '(hl-line ((t (:background "grey15"))))
  '(linum ((t (:inherit shadow :background "grey15"))))
