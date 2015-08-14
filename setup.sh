ed9ab475958e5f96d33c53e576d45a258f4bf0d7
Paul <paul@ruthorn.co.uk>
2015-08-14 13:29:43 +0100

    Note time of last `apt-get update` and only perform in setup.sh if needed

--- setup.sh
+++ setup.sh
@@ -2,8 +2,24 @@
 
 this_dir=$(dirname $0)
 
-sudo apt-get update
-sudo apt-get upgrade -y
+apt_cache_update_file=/var/lib/apt/periodic/update-success-stamp
+update_apt=1
+
+if [[ -e $apt_cache_update_file ]]; then
+    apt_cache_updated=$(stat -c %Y /var/lib/apt/periodic/update-success-stamp)
+    now=$(date +%s)
+    let 'day_seconds = 60 * 60 * 24'
+    let 'apt_cache_age = now - apt_cache_updated'
+    if (( $apt_cache_age < $day_seconds )); then
+        update_apt=0
+    fi
+fi
+
+if (( $update_apt == 1 )); then
+    sudo apt-get update
+    sudo apt-get upgrade -y
+fi
+
 sudo apt-get install -y python-pip python-dev aptitude
 sudo pip install ansible markupsafe passlib
 
